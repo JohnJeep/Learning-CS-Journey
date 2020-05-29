73545c19cc5e2d7e27c67673c23d08f573d61a3c
JohnJeep <johnjeep1985@gmail.com>
2020-05-29 16:19:23 +0800

    Alter register.cpp, const.c and const.cpp files

---
+++ C++/code/const.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-29 11:45:46
+ * @LastEditTime: 2020-05-29 12:20:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ */ 
+#include <iostream>
+
+int main()
+{
+    const int a = 10;
+    int *b = NULL;
+    b = (int*)&a;     
+    printf("a before value: %d\n", a);
+    printf("a before address: 0x%p\n", &a);
+    printf("b before value: %d\n", *b);
+    printf("b before address: 0x%p\n", b);
+    printf("\n");
+       
+    *b = 20;      // C++中 保证了const声明的变量绝对不能修改  
+    printf("a after value: %d\n", a);
+    printf("a after address: 0x%p\n", &a);
+    printf("b after value: %d\n", *b);
+    printf("b after address: 0x%p\n", b);
+
+    return 0;
+}
