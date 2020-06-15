e36c7eef49b016f24513ff0f7c421ec5d1a10607
JohnJeep <JohnJeep1985@gmail.com>
2020-06-15 16:20:34 +0800

    Add template_func.cpp and template_func_param.cpp.

---
+++ C++/code/template_func.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Author: JohnJeep
+ * @Date: 2020-06-15 09:06:47
+ * @LastEditTime: 2020-06-15 09:36:40
+ * @LastEditors: Please set LastEditors
+ * @Description: 模板函数用例
+ * @FilePath: /C++/code/template_func.cpp
+ */ 
+#include <iostream>
+using namespace std;
+
+// 采用引用的方法交换两个数
+template <typename T>
+void tswap(T &a, T &b)
+{
+    T c;
+    c = a;
+    a = b;
+    b = c;
+}
+
+int main()
+{
+    int x = 11; 
+    int y = 22; 
+    tswap<int>(x, y);    // 显示调用
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+
+    char a = 'A'; 
+    char b = 'B'; 
+    tswap(a, b);       // 调用时自动类型推导
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    return 0;
+}
---
+++ C++/code/template_func_param.cpp
@@ -0,0 +1,83 @@
+/*
+ * @Author: JohnJeep
+ * @Date: 2020-06-15 10:13:51
+ * @LastEditTime: 2020-06-15 11:13:10
+ * @LastEditors: Please set LastEditors
+ * @Description: 函数模板做函数参数
+ * @FilePath: /C++/code/template_func_param.cpp
+ */ 
+#include <iostream>
+using namespace std;
+
+
+// 数组排序，不采用模板函数
+void arraySort(int *array, int len)
+{
+    int tmp;
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if(array[i] > array[j])
+            {              
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }           
+        }
+    }
+}
+
+// 数组排序，采用模板函数
+template <typename T1, typename T2>
+void arraySortTemplate(T1 *array, T2 len)
+{
+    T1 tmp;
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            tmp = array[i];
+            if(array[i] > array[j])
+            {              
+                array[i] = array[j];
+                array[j] = tmp;
+            }           
+        }
+    }
+}
+
+
+int main()
+{
+    int array[] = {100, 98, 30, 200, 150};
+    int length = sizeof(array) / sizeof(array[0]);
+    
+    arraySort(array, length);
+    for (int i = 0; i < length; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    arraySortTemplate<int, int>(array, length);
+    for (int k = 0; k < length; k++)
+    {
+        cout << array[k] << " ";
+    }
+    cout << endl;
+
+    char pArray[] = {'A', 'Z', 'Y', 'B', 'F'}; 
+    int pLen = sizeof(pArray) / sizeof(pArray[0]);
+    arraySortTemplate<char, int>(pArray, length);
+    for (int s = 0; s < pLen; s++)
+    {
+        cout << pArray[s] << " ";
+    }
+
+    return 0;
+}
