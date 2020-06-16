2776637fbd2b8b1432b3467b694ff464cabaf226
JohnJeep <JohnJeep1985@gmail.com>
2020-06-16 16:19:36 +0800

    Append class template and function template.

--- C++/code/hello.cpp
+++ C++/code/hello.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-27 08:55:46
- * @LastEditTime: 2020-05-27 10:04:37
+ * @LastEditTime: 2020-06-16 14:39:30
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Learning-Computer\C++\code\hello.cpp
@@ -9,14 +9,21 @@
 #include <stdio.h>
 #include <iostream>
 
+template <typename T>
+T add(T x, T y)
+{
+    return x + y;
+}
+
 int main()
 {
     using namespace std;
     
-    int a =10;
-    cout << a;
-    cout << endl;
-    system("pause");
+    int a = 10;
+    int b = 20;
+
+    cout << add(a, b) << endl;
+    cout << add(12.5, 10.2) << endl;
     
     return 0;
 }
