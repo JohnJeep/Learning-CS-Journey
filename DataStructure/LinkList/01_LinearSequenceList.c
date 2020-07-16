6a4845fd083153a79d61813d33b4b1ee4f6123be
JohnJeep <JohnJeep1985@gmail.com>
2020-07-16 23:10:35 +0800

    modified:   DataStructure/LinkList/01_LinearSequenceList.c new file:   DataStructure/LinkList/02_LinearChainList.c

--- DataStructure/LinkList/01_LinearSequenceList.c
+++ DataStructure/LinkList/01_LinearSequenceList.c
@@ -1,7 +1,7 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-07-15 19:57:19
- * @LastEditTime: 2020-07-15 23:26:15
+ * @LastEditTime: 2020-07-16 22:02:32
  * @LastEditors: Please set LastEditors
  * @Description: 线性表顺序存储
  * @FilePath: /01_LinearSequenceList.c
@@ -15,14 +15,21 @@
 typedef struct tag_sqlist
 {
     int length;
-    int data[MAX_SIZE];
-    // int capacity;
-    // unsigned int** node;    // 动态分配内存空间
+    // int data[MAX_SIZE];
+    int capacity;
+    unsigned int** node;    // 动态分配内存空间
 }SqList;
 
+typedef void SqListNode;
+
+typedef struct tag_teacher
+{
+    int age;
+    char *name;
+}Teacher;
 
 
-#if 0
+#if 1
 /**
  * @description: 线性表的创建
  * @param {type} 
@@ -35,47 +42,51 @@ SqList* sqListCreate(int size)
     if (pstr == NULL)
     {
         printf("pstr error.\n");
-        return -1;
+        return NULL;
     }
 
     memset(pstr, 0, sizeof(SqList));
     
     // 根据size空间的大小分配节点空间的大小
-    pstr->node = (unsigned int*)malloc(sizeof(unsigned int*) * size);
+    pstr->node = (unsigned int**)malloc(sizeof(unsigned int**) * size);
     if (pstr->node == NULL)
     {   
         printf("pstr->node malloc space error.\n");    
-        return -1;
+        // return -1;
+        return NULL;
     }
     pstr->capacity = size;
     pstr->length = 0;
     return pstr;
-    
 }
 
 /**
- * @description: 线性表的销毁
+ * @description: 线性表的销毁: 两次开辟了内存空间，释放内存空间两次
  * @param {type} 
  * @return: 
  */
 void sqListDestory(SqList* list)
 {
-    SqList* p = NULL;
     if (list == NULL)
     {
         return;
     }
-    if (p->node != NULL)
+    if (list->node != NULL)
     {
-        free(p->node);
+        free(list->node);
     }
     
-    free(p);
+    free(list);
 }
 
+/**
+ * @description: 清空链表
+ * @param {type} 
+ * @return: 
+ */
 void sqListClear(SqList* list)
 {
-    if (list != NULL)
+    if (list == NULL)
     {
         return;
     }
@@ -89,7 +100,7 @@ void sqListClear(SqList* list)
  */
 int sqListLength(SqList* list)
 {
-    if (list != NULL)
+    if (list == NULL)
     {
         return -1;
     }
@@ -110,10 +121,76 @@ int sqListCapacity(SqList* list)
  * @param {type} 
  * @return: 
  */
-SqList* sqListInsert(SqList *list)
+int sqListInsert(SqList *list, SqListNode* node, int pos)
 {
+    int i;
 
+    if (list == NULL || node == NULL || pos < 0)
+    {
+        return -1;
+    }
+
+    if (list->length >= list->capacity)
+    {
+        printf("capacity is filled.\n");
+        return -1;
+    }
+
+    // 容错修正
+    if (pos > list->length)
+    {
+        pos = list->length;
+    }
+     
+    for (i = list->length; i > pos; i--) // 将要插入位置的元素向后移动一位
+    {
+        list->node[i] = list->node[i-1];
+    }
+    list->node[i] = (unsigned int*)node;// 向节点处插入元素
+
+    list->length++;
+    return 0;
 }
+
+/**
+ * @description: 获取指定元素的位置
+ * @param {type} 
+ * @return: 
+ */
+SqList* sqListGetLen(SqList* list, int pos)
+{
+    if (list == NULL || pos < 0)
+    {
+        return NULL;
+    }
+    
+    return (SqList*)list->node[pos];
+}
+
+/**
+ * @description: 线性表的删除
+ * @param {type} 
+ * @return: 
+ */
+int sqListDelete(SqList* list, int pos)
+{
+    int i, ret;
+    if (list == NULL || pos < 0)
+    {
+        return -1;
+    }
+
+    // *ret = list->node[pos];
+    for ( i = pos + 1; i < list->length; i++)
+    {
+        list->node[i] = list->node[i+1];
+    }
+    list->length--;
+    return 0;
+}
+ 
+
+
 #endif 
 
 
@@ -122,6 +199,28 @@ SqList* sqListInsert(SqList *list)
 
 int main(int argc, char *argv[])
 {
+    SqList* p = NULL;
+    int ret;
     
+    Teacher t1, t2, t3;
+    t1.age = 20;
+    t2.age = 21;
+    t3.age = 22;
+    p = sqListCreate(16); 
+    ret = sqListInsert(p, (SqListNode*)&t1, 0);
+    ret = sqListInsert(p, (SqListNode*)&t2, 0);
+    ret = sqListInsert(p, (SqListNode*)&t3, 0);
+
+    for (int i = 0; i < sqListLength(p); i++)
+    {
+        Teacher* te = (Teacher*)sqListGetLen(p, i);
+        if (te != NULL)
+        {
+            printf("te->age:%d ", te->age);
+        }
+        printf("\n");
+        
+    }
+
     return 0;
 }
---
+++ DataStructure/LinkList/02_LinearChainList.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: JohnJeep
+ * @Date: 2020-07-16 22:03:51
+ * @LastEditTime: 2020-07-16 22:05:11
+ * @LastEditors: Please set LastEditors
+ * @Description: 线性表的链式存储
+ * @FilePath: /02_LinearChainList.c
+ */ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
+
