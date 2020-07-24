c0a205541246223eea7d1d65460ae168e263aff1
JohnJeep <JohnJeep1985@gmail.com>
2020-07-24 16:07:13 +0800

    modified DataStructure files

--- DataStructure/StackHeap/01_stack.h
+++ DataStructure/StackHeap/01_stack.h
@@ -1,7 +1,7 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-07-22 21:20:00
- * @LastEditTime: 2020-07-22 21:39:30
+ * @LastEditTime: 2020-07-24 11:13:02
  * @LastEditors: Please set LastEditors
  * @Description: stack的底层接口
  * @FilePath: /01_stack.h
@@ -9,12 +9,22 @@
 #ifndef __01_STACK_H
 #define __01_STACK_H
 
+typedef int elemType;
+typedef struct tag_sqStack
+{
+    elemType *bottom;
+    elemType *top;
+    int stackSize;
+}sqStack;
+
+
+
 typedef void Stack;
 void stackInit();                            // 栈的初始化
 void stackDestory(Stack* stack);             // 销毁栈
 void* stackTop(Stack* stack);                // 取栈顶元素
 int stackPush(Stack* stack, void* iterm);    // 进栈
-void* stackPop(stack* stack);                // 出栈
+void* stackPop(Stack* stack);                // 出栈
 void stackClear(Stack* stack);               // 清空栈
 int stackSize(Stack* stack);                 // 获取栈的大小
 
