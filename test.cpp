6e5426585822d71d0e1d1b10aef416c8c19086a8
JohnJeep <JohnJeep1985@gmail.com>
2020-08-13 16:19:13 +0800

    Append some contents on Git and C++ files

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-13 16:06:32
+ * @LastEditTime: 2020-08-13 16:07:28
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \Learning-Computer-Journey\test.cpp
+ */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct test
+{
+    char a;
+    int b;
+};
+
+int main(int argc, char *argv[])
+{
+    cout << sizeof(test) << endl;
+    return 0;
+}
