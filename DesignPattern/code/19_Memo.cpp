c71e93f2fc1c92fa2eb4f2b00c45862ae2ad3a85
JohnJeep <JohnJeep1985@gmail.com>
2020-09-18 16:18:04 +0800

    Add Visitor, State, Interpreter, Iterator pattern

--- DesignPattern/code/19_Memo.cpp
+++ DesignPattern/code/19_Memo.cpp
@@ -1,15 +1,31 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-09-17 16:10:52
- * @LastEditTime: 2020-09-17 16:19:05
+ * @LastEditTime: 2020-09-18 14:44:53
  * @LastEditors: Please set LastEditors
- * @Description: 备忘录模式
+ * @Description: 备忘录模式实现
  * 
  */
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
+
+class Memo
+{
+private:
+    string s_name;
+    int s_age;
+public:
+    Memo(string name, int age) 
+        : s_name(name), s_age(age) 
+    {}
+    ~Memo() 
+    {}
+
+    string getName() const {return s_name;}
+    int getAge() const {return s_age;}
+};
 
 class Person
 {
@@ -20,19 +36,42 @@ public:
     Person(string name, int age) 
         : m_name(name), m_age(age)
     {}
-    ~Person() {}
+    ~Person() 
+    {}
+
+    void setName(string name) {this->m_name = name;}
+    void setAge(int age) {this->m_age = age;}
+    string getName() const {return m_name;}
+    int getAge() const {return m_age;}
+    Memo* saveMemo()
+    {
+        return new Memo(m_name, m_age);
+    }
+    void recoverMemo(Memo* mo)
+    {
+        this->m_name = mo->getName();
+        this->m_age = mo->getAge();
+    }
+    void showInfo()
+    {
+        cout << "name:" << m_name << "\t" << "age: " << m_age << endl;
+    }
 };
 
-class Memo
-{
-private:
-    /* data */
-public:
-    Memo(/* args */) {}
-    ~Memo() {}
-};
 int main(int argc, char *argv[])
 {
+    Person* p = new Person("Jade", 24);
+    p->showInfo();
     
+    Memo* onenote = p->saveMemo();  // 将Person对象中的数据保存到备忘录对象中
+    p->setAge(30);                  // 改变Person对象中的数据
+    p->setName("Anna");
+    p->showInfo();
+    
+    p->recoverMemo(onenote);        // 从备忘录对象中恢复数据 
+    p->showInfo();
+    delete p;
+    delete onenote;
+
     return 0;
 }
