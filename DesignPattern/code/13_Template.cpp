463e4f522ad1ccce147b071bf58a9b6e099a1f46
JohnJeep <JohnJeep1985@gmail.com>
2020-12-24 23:06:50 +0800

    Modified Template Method

--- DesignPattern/code/13_Template.cpp
+++ DesignPattern/code/13_Template.cpp
@@ -1,13 +1,13 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-09-16 11:39:23
- * @LastEditTime: 2020-09-16 12:07:19
+ * @LastEditTime: 2020-12-24 22:59:20
  * @LastEditors: Please set LastEditors
  * @Description: 模板模式：父类的抽象类定义方法，在子类中去实现
  * 
   */
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
@@ -18,10 +18,16 @@ private:
 public:
     AbstactAnimal(/* args */) {}
     virtual ~AbstactAnimal() {}
+
+    // 可变的部分，需要子类中实现
     virtual void flying() = 0;
     virtual void running() = 0;
 
-    void action()   // 模板函数把业务结点确定好了
+    /**
+   * The template method defines the skeleton of an algorithm.
+   *  stable part.
+   */
+    void action() // 模板函数把业务结点确定好了
     {
         flying();
         running();
@@ -35,6 +41,7 @@ private:
 public:
     Bird(/* args */) {}
     ~Bird() {}
+
     virtual void flying()
     {
         cout << "I am bird, I am flying" << endl;
@@ -42,7 +49,7 @@ public:
     virtual void running()
     {
         cout << "I am bird, I am running" << endl;
-    }    
+    }
 };
 
 class Mammal : public AbstactAnimal
@@ -52,6 +59,7 @@ private:
 public:
     Mammal(/* args */) {}
     ~Mammal() {}
+
     virtual void running()
     {
         cout << "I am lion, I am running" << endl;
@@ -59,19 +67,18 @@ public:
     virtual void flying()
     {
         cout << "I am lion, I am flying" << endl;
-    }    
+    }
 };
 
 int main(int argc, char *argv[])
 {
-    AbstactAnimal* parrot = new Bird;
-    parrot->action();   // 直接调用抽象类中的模板函数
+    AbstactAnimal *parrot = new Bird;
+    parrot->action(); // 直接调用抽象类中的模板函数
     delete parrot;
 
-    AbstactAnimal* lion = new Mammal;
+    AbstactAnimal *lion = new Mammal;
     lion->action();
     delete lion;
 
-    
     return 0;
 }
