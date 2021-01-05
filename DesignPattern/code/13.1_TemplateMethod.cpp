407269b9e41652ee80ba572724270b4589594b26
JohnJeep <JohnJeep1985@gmail.com>
2021-01-05 16:21:57 +0800

    add design pattern

--- DesignPattern/code/13.1_TemplateMethod.cpp
+++ DesignPattern/code/13.1_TemplateMethod.cpp
@@ -1,9 +1,9 @@
 /*
- * @Author: your name
+ * @Author: JohnJeep
  * @Date: 2020-12-24 22:42:04
- * @LastEditTime: 2020-12-24 23:05:43
+ * @LastEditTime: 2021-01-04 14:52:08
  * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
+ * @Description: Template method implement
  */
 #include <iostream>
 #include <stdlib.h>
@@ -17,12 +17,15 @@ using namespace std;
  * Concrete subclasses should implement these operations, but leave the template
  * method itself intact.
  */
-class AbstractClass {
+class AbstractClass
+{
+
   /**
    * The template method defines the skeleton of an algorithm.
    */
- public:
-  void TemplateMethod() const {
+public:
+  void TemplateMethod() const
+  {
     this->BaseOperation1();
     this->RequiredOperations1();
     this->BaseOperation2();
@@ -31,24 +34,34 @@ class AbstractClass {
     this->BaseOperation3();
     this->Hook2();
   }
+
+
   /**
    * These operations already have implementations.
    */
- protected:
-  void BaseOperation1() const {
+protected:
+  void BaseOperation1() const
+  {
     std::cout << "AbstractClass says: I am doing the bulk of the work\n";
   }
-  void BaseOperation2() const {
+
+  void BaseOperation2() const
+  {
     std::cout << "AbstractClass says: But I let subclasses override some operations\n";
   }
-  void BaseOperation3() const {
+
+  void BaseOperation3() const
+  {
     std::cout << "AbstractClass says: But I am doing the bulk of the work anyway\n";
   }
+
+
   /**
    * These operations have to be implemented in subclasses.
    */
   virtual void RequiredOperations1() const = 0;
   virtual void RequiredOperation2() const = 0;
+
   /**
    * These are "hooks." Subclasses may override them, but it's not mandatory
    * since the hooks already have default (but empty) implementation. Hooks
@@ -58,53 +71,73 @@ class AbstractClass {
   virtual void Hook1() const {}
   virtual void Hook2() const {}
 };
+
+
 /**
  * Concrete classes have to implement all abstract operations of the base class.
  * They can also override some operations with a default implementation.
  */
-class ConcreteClass1 : public AbstractClass {
- protected:
-  void RequiredOperations1() const override {
+class ConcreteClass1 : public AbstractClass
+{
+protected:
+  void RequiredOperations1() const override
+  {
     std::cout << "ConcreteClass1 says: Implemented Operation1\n";
   }
-  void RequiredOperation2() const override {
+
+  void RequiredOperation2() const override
+  {
     std::cout << "ConcreteClass1 says: Implemented Operation2\n";
   }
 };
+
+
 /**
  * Usually, concrete classes override only a fraction of base class' operations.
  */
-class ConcreteClass2 : public AbstractClass {
- protected:
-  void RequiredOperations1() const override {
+class ConcreteClass2 : public AbstractClass
+{
+protected:
+  void RequiredOperations1() const override
+  {
     std::cout << "ConcreteClass2 says: Implemented Operation1\n";
   }
-  void RequiredOperation2() const override {
+
+  void RequiredOperation2() const override
+  {
     std::cout << "ConcreteClass2 says: Implemented Operation2\n";
   }
-  void Hook1() const override {
+
+  void Hook1() const override
+  {
     std::cout << "ConcreteClass2 says: Overridden Hook1\n";
   }
 };
+
+
 /**
  * The client code calls the template method to execute the algorithm. Client
  * code does not have to know the concrete class of an object it works with, as
  * long as it works with objects through the interface of their base class.
  */
-void ClientCode(AbstractClass *class_) {
+void ClientCode(AbstractClass *class_)
+{
   // ...
   class_->TemplateMethod();
   // ...
 }
 
-int main() {
+int main()
+{
   std::cout << "Same client code can work with different subclasses:\n";
   ConcreteClass1 *concreteClass1 = new ConcreteClass1;
   ClientCode(concreteClass1);
   std::cout << "\n";
+  
   std::cout << "Same client code can work with different subclasses:\n";
   ConcreteClass2 *concreteClass2 = new ConcreteClass2;
   ClientCode(concreteClass2);
+  
   delete concreteClass1;
   delete concreteClass2;
   return 0;
