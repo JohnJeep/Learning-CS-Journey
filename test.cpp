9c2c50d7f8d60128bf00071f72f14a739ea11f4d
JohnJeep <JohnJeep1985@gmail.com>
2021-02-05 16:17:09 +0800

    Update C++NewFeature, stl_test_containers.cpp

---
+++ test.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-05 08:49:10
+ * @LastEditTime: 2021-02-05 11:21:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Learning-Computer-Science-Journey\test.cpp
+ */
+#include <iostream>
+#include <cstdio>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    size_t iter;
+
+    cout << sizeof(long) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long long) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    return 0;
+}
