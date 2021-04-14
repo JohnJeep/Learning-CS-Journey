645e2c72812b6df1a03920c0b177b94d71cbbc47
JohnJeep <JohnJeep1985@gmail.com>
2021-04-15 01:09:13 +0800

    Update sort-an-array

---
+++ Interview/LeetCode/1291.sequential-digits.cpp
@@ -0,0 +1,55 @@
+/*
+ * @Author: JohnJeep
+ * @Date: 2021-04-15 01:01:46
+ * @LastEditTime: 2021-04-15 01:08:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ */
+
+/*
+ * @lc app=leetcode id=1291 lang=cpp
+ *
+ * [1291] Sequential Digits
+ *
+ * https://leetcode.com/problems/sequential-digits/description/
+ *
+ * algorithms
+ * Medium (57.38%)
+ * Likes:    534
+ * Dislikes: 55
+ * Total Accepted:    37.2K
+ * Total Submissions: 64.9K
+ * Testcase Example:  '100\n300'
+ *
+ * An integer has sequential digits if and only if each digit in the number is
+ * one more than the previous digit.
+ * 
+ * Return a sorted list of all the integers in the range [low, high] inclusive
+ * that have sequential digits.
+ * 
+ * 
+ * Example 1:
+ * Input: low = 100, high = 300
+ * Output: [123,234]
+ * Example 2:
+ * Input: low = 1000, high = 13000
+ * Output: [1234,2345,3456,4567,5678,6789,12345]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 10 <= low <= high <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+    
+    }  
+};
+// @lc code=end
+
