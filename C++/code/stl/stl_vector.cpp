5a14575231b8b92eabc8ac305d797c0999e09c89
JohnJeep <johnjeep1985@gmail.com>
2021-05-21 16:21:52 +0800

    Update stl vector and list files format

--- C++/code/stl/stl_vector.cpp
+++ C++/code/stl/stl_vector.cpp
@@ -1,10 +1,9 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-07-16 10:48:40
- * @LastEditTime: 2020-07-22 08:43:44
+ * @LastEditTime: 2021-05-21 16:20:59
  * @LastEditors: Please set LastEditors
  * @Description: STL标准库中vector常见用法
- * @FilePath: /34_stl_vector.cpp
  */ 
 #include <iostream>
 #include <cstdio>
@@ -24,8 +23,7 @@ void test01()
     cout << "push element after, vct1 size: " << vct1.size() << endl;
 
     vct1.pop_back();
-    for (size_t i = 0; i < vct1.size(); i++)
-    {
+    for (size_t i = 0; i < vct1.size(); i++) {
         cout << "vector element: " << vct1[i] << endl;  // 通过数组的方式访问vector
     }
     cout << endl;
@@ -34,8 +32,7 @@ void test01()
     vct1.front() = 10;
     vct1.back() = 20;
 
-    for (size_t j = 0; j < vct1.size(); j++)
-    {
+    for (size_t j = 0; j < vct1.size(); j++) {
         cout << "amend vector element aafter: " << vct1[j] << endl;
     }
     cout << endl;
@@ -45,23 +42,20 @@ void test01()
 void test02()
 {
     vector<int> vct2(10);   // 给vector分配 10 个 int 类型的内存空间
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         vct2[i] = i + 1;
     }
 
     // 正向遍历
     cout << "迭代器正向遍历: ";
-    for (vector<int>::iterator it = vct2.begin(); it != vct2.end(); it++)
-    {
+    for (vector<int>::iterator it = vct2.begin(); it != vct2.end(); it++) {
         cout << *it << " ";
     }
     cout << endl;
 
     // 反向遍历
     cout << "迭代器反向遍历: ";
-    for (vector<int>::reverse_iterator rit = vct2.rbegin(); rit != vct2.rend(); rit++)
-    {
+    for (vector<int>::reverse_iterator rit = vct2.rbegin(); rit != vct2.rend(); rit++) {
         cout << *rit << " ";
     }
     cout << endl;
@@ -72,8 +66,7 @@ void test03()
 {
     vector<int> vct2(10);   // 给vector分配 10 个 int 类型的内存空间
     cout << "origin vector element: ";
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         vct2[i] = i + 1;
         cout << vct2[i] << " ";
     }
@@ -82,8 +75,7 @@ void test03()
     vct2.erase(vct2.begin()); // 删除指定位置的值
     vct2.erase(vct2.begin() + 3, vct2.begin() + 5);   // 删除指定区间 [3, 5) 的值
     cout << "delete specific region vector element: ";
-    for (size_t i = 0; i < vct2.size(); i++)
-    {
+    for (size_t i = 0; i < vct2.size(); i++) {
         cout << vct2[i] << " ";  // 通过数组的方式访问vector
     }
     cout << endl; 
@@ -94,35 +86,33 @@ void test03()
     vct2.push_back(5);
     vct2.push_back(5);
     cout << "vector element: ";
-    for (size_t i = 0; i < vct2.size(); i++)
-    {
+    for (size_t i = 0; i < vct2.size(); i++) {
         cout << vct2[i] << " ";  // 通过数组的方式访问vector
     }
     cout << endl;
 
     // 删除vector中指定的元素值
-    for (vector<int>::iterator it = vct2.begin(); it != vct2.end();)
-    {
-        if (*it == 5)
-        {
+    for (vector<int>::iterator it = vct2.begin(); it != vct2.end()) {
+        if (*it == 5) {
            it = vct2.erase(it);   // 返回值为下一个元素的首地址
         }
-        else
-        {
+        else {
             it++;
         }
     }
     cout << "delete vector element: ";
-    for (size_t i = 0; i < vct2.size(); i++)
-    {
+    for (size_t i = 0; i < vct2.size(); i++) {
         cout << vct2[i] << " ";  // 通过数组的方式访问vector
     }
     cout << endl;
 }
+
+
 int main(int argc, char *argv[])
 {
     // test01();
     // test02();
     test03();
+
     return 0;
 }
--- C++/code/stl/stl_vector_size.cpp
+++ C++/code/stl/stl_vector_size.cpp
@@ -1,10 +1,9 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-07-26 11:17:53
- * @LastEditTime: 2020-07-26 11:38:52
+ * @LastEditTime: 2021-05-21 16:15:31
  * @LastEditors: Please set LastEditors
  * @Description: size()与capacity()STL库函数的区别
- * @FilePath: /44_vector_size.cpp
  */ 
 #include <iostream>
 #include <stdlib.h>
@@ -14,12 +13,10 @@ using namespace std;
 
 void show(vector<int>& v)
 {
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         v[i] = i + 1;
     }    
-    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    {
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
         cout << *it << " ";
     }
     cout << endl;    
@@ -34,7 +31,9 @@ int main(int argc, char *argv[])
 
     cout << "size = " << vt.size() << "\t" << "capicity = " << vt.capacity() << endl;
 
-    vt.reserve(10);   // 容器中预留的内存空间，并没有真正分配内存空间，即此时空间是野的。此时使用[]操作符访问容器内的对象，很可能出现数组越界的问题。
+    // 容器中预留的内存空间，并没有真正分配内存空间，即此时空间是野的。
+    // 此时使用[]操作符访问容器内的对象，很可能出现数组越界的问题。
+    vt.reserve(10);   
     cout << "size = " << vt.size() << "\t" << "capicity = " << vt.capacity() << endl;
 
     cout << "execute reserve: ";
