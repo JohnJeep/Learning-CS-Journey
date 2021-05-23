02b76c3b6526d462f021c5f6ae86a4c9536d5996
JohnJeep <JohnJeep1985@gmail.com>
2021-05-23 23:53:54 +0800

    Rename struct-byte-alihn, custom-string-copy files

--- C/code/custom-string-copy.c
+++ C/code/custom-string-copy.c
@@ -1,10 +1,9 @@
 /*
  * @Author: JonJeep
  * @Date: 2020-08-17 10:57:56
- * @LastEditTime: 2020-08-17 14:24:14
+ * @LastEditTime: 2021-05-23 17:52:05
  * @LastEditors: Please set LastEditors
  * @Description: 自定义实现字符串的拷贝函数
- * @FilePath: /custom-strcopy.c
  */
 #include <stdio.h>
 #include <string.h>
@@ -13,39 +12,35 @@
 void str_cpy_1(char *dest, const char *src)
 {
     int i = 0;
-    for (i = 0; *(src+i) != '\0'; i++)
-    {
-        *(dest + i) = *(src + i);   // 等价于dest[i] = src[i];
+    for (i = 0; *(src + i) != '\0'; i++) {
+        *(dest + i) = *(src + i); // 等价于dest[i] = src[i];
     }
     *(dest + i) = '\0';
 }
 
 void str_cpy_2(char *dest, const char *src)
 {
-    while (*src != '\0')
-    {
-        *dest = *src;    // src首地址的值赋给dest首地址的值
+    while (*src != '\0') {
+        *dest = *src; // src首地址的值赋给dest首地址的值
         dest++;
         src++;
     }
-    *dest = '\0';        // 最后一位设置为\0
+    *dest = '\0'; // 最后一位设置为\0
 }
 
 void str_cpy_3(char *dest, const char *src)
 {
-    while (*src != '\0')
-    {
-                            // * 与++的优先级在同一级，从右向左运算，但dest++是先运算后再执行其它运算。
-        *dest++ = *src++;   // 先执行*dest = *src,再执行dest++，src++
+    while (*src != '\0') {
+        // * 与++的优先级在同一级，从右向左运算，但dest++是先运算后再执行其它运算。
+        *dest++ = *src++; // 先执行*dest = *src,再执行dest++，src++
     }
     *dest = '\0';
 }
 
 void str_cpy_4(char *dest, const char *src)
 {
-    //while (*dest++ = *src++)  
-    while ((*dest++ = *src++) != '\0')     
-    {
+    //while (*dest++ = *src++)
+    while ((*dest++ = *src++) != '\0') {
         // 执行顺序：先执行*dest = *src,再执行dest++，src++
         // 判断*dest是否等于0，若等于0，则跳出循环。
         ;
@@ -58,20 +53,17 @@ int str_cpy_5(char *dest, const char *src)
     char *d = dest;
     const char *s = src;
 
-    if((d == NULL) | (s == NULL))
-    {
+    if ((d == NULL) | (s == NULL)) {
         printf("pointer is null:error! \n");
         return -1;
     }
-    while ((*d++ = *s++) != '\0')     
-    {
+    while ((*d++ = *s++) != '\0') {
         ;
     }
-    *d = '\0';    
-  
+    *d = '\0';
+
     return 0;
 }
-
 
 int main()
 {
@@ -99,16 +91,14 @@ int main()
     printf("\n");
 
     int tmp = str_cpy_5(dest, src);
-    if (tmp == 0)
-    {
+    if (tmp == 0) {
         printf("dest_5=%s \n", dest);
         printf("src_5 =%s \n", src);
         printf("\n");
     }
-    else
-    {
+    else {
         printf("program error! \n");
     }
-    
-   return 0;
+
+    return 0;
 }
--- C/code/struct-byte-align.c
+++ C/code/struct-byte-align.c
@@ -1,40 +1,36 @@
 /*
- * @Author: your name
+ * @Author: JohnJeep
  * @Date: 2020-03-02 15:38:07
- * @LastEditTime: 2020-03-02 16:18:34
+ * @LastEditTime: 2021-05-23 17:50:51
  * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \C\struct_byteAlignment.c
+ * @Description: 结构体中字节对齐
  */
+#include <malloc.h>
 #include <stdio.h>
 #include <string.h>
-#include <malloc.h>
 
 // 字节对齐按照结构体成员中最长的成员对齐
 
-#pragma pack()     // 指定特定的字节对齐
-struct 
+#pragma pack() // 指定特定的字节对齐
+struct
 {
     int age;
     char name;
     short id;
-}nurse;
+} nurse;
 
-struct 
+struct
 {
     char name;
     int age;
     short id;
-}nurse_1;
+} nurse_1;
 
 int main()
 {
     printf("%d\n", sizeof(nurse));
     printf("%d\n", sizeof(nurse_1));
     getchar();
+
     return 0;
 }
-
-
-
-
