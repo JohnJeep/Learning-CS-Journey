34b0df5d7c04391f990e32e4d9db539a18e9bb64
JohnJeep <JohnJeep1985@gmail.com>
2021-08-03 15:08:00 +0800

    Append c11 Chrono module, threadpool

---
+++ C++/ThreadPool.md
@@ -0,0 +1,9 @@
+<!--
+ * @Author: JohnJeep
+ * @Date: 2021-08-03 14:58:43
+ * @LastEditTime: 2021-08-03 15:05:51
+ * @LastEditors: Please set LastEditors
+ * @Description: 线程池工作原理
+-->
+# 参考
+- [基于C++11实现线程池的工作原理](https://www.cnblogs.com/ailumiyana/p/10016965.html)
--- C++/code/c11/chrono.cpp
+++ C++/code/c11/chrono.cpp
@@ -1,14 +1,24 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-12-10 11:48:06
- * @LastEditTime: 2021-01-25 20:05:54
+ * @LastEditTime: 2021-07-26 16:43:21
  * @LastEditors: Please set LastEditors
  * @Description: chrono的使用
  */
 #include <iostream>
 #include <ratio>
 #include <chrono>
+
 using namespace std::chrono;
+
+// 定义实现 << 运算符
+template <typename V, typename R>
+ostream& operator << (ostream& s, const chrono::duration<V,R>& d)
+{
+	s << "[" << d.count() << " of " << R::num << "/"
+	<< R::den << "]";
+	return s;
+}
 
 // time_point有一个函数time_from_eproch()用来获得1970年1月1日到time_point时间经过的duration。
 // 举个例子，如果timepoint以天为单位，函数返回的duration就以天为单位。
@@ -24,5 +34,8 @@ int main()
         
     std::cout << today.time_since_epoch().count() << " days since epoch" << std::endl;
 
+	std::chrono::microseconds s(40);
+	std::cout << s << endl;
+	
     return 0;
 }
