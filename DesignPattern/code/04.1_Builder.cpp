c9cf3bd9b5f0b06378fb596a3746fdf5b1039c82
JohnJeep <JohnJeep1985@gail.com>
2022-07-08 18:06:28 +0800

    Update observer pattern and docs

--- DesignPattern/code/04.1_Builder.cpp
+++ DesignPattern/code/04.1_Builder.cpp
@@ -1,143 +1,156 @@
 /*
  * @Author: JohnJeep
  * @Date: 2020-12-23 21:10:27
- * @LastEditTime: 2020-12-23 23:09:27
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-07-08 10:57:54
+ * @LastEditors: DESKTOP-0S33AUT
  * @Description: 重新理解Builder
  */
+#include <cstring>
 #include <iostream>
 #include <stdlib.h>
-#include <cstring>
 
 using namespace std;
 
 // Product
 class Pizza {
 public:
-        void dough(const string& dough) {
-                dough_ = dough;
-        }
- 
-        void sauce(const string& sauce) {
-                sauce_ = sauce;
-        }
- 
-        void topping(const string& topping) {
-                topping_ = topping;
-        }
- 
-        void open() const {
-                cout << "Pizza with " << dough_ << " dough, " << sauce_ << " sauce and "
-                        << topping_ << " topping. Mmm." << endl;
-        }
- 
+    void dough(const string& dough)
+    {
+        dough_ = dough;
+    }
+
+    void sauce(const string& sauce)
+    {
+        sauce_ = sauce;
+    }
+
+    void topping(const string& topping)
+    {
+        topping_ = topping;
+    }
+
+    void open() const
+    {
+        cout << "Pizza with " << dough_ << " dough, " << sauce_ << " sauce and "
+             << topping_ << " topping. Mmm." << endl;
+    }
+
 private:
-        string dough_;
-        string sauce_;
-        string topping_;
+    string dough_;
+    string sauce_;
+    string topping_;
 };
- 
+
 // Abstract Builder
 class PizzaBuilder {
 public:
-       // Chinmay Mandal : This default constructor may not be required here
-        PizzaBuilder()
-        {
-           // Chinmay Mandal : Wrong syntax
-           // pizza_ = new Pizza;
-        }
-        const Pizza& pizza() {
-                return pizza_;
-        }
- 
-        virtual void buildDough() = 0;
-        virtual void buildSauce() = 0;
-        virtual void buildTopping() = 0;
- 
+    // Chinmay Mandal : This default constructor may not be required here
+    PizzaBuilder()
+    {
+        // Chinmay Mandal : Wrong syntax
+        // pizza_ = new Pizza;
+    }
+    const Pizza& pizza()
+    {
+        return pizza_;
+    }
+
+    virtual void buildDough() = 0;
+    virtual void buildSauce() = 0;
+    virtual void buildTopping() = 0;
+
 protected:
-        Pizza pizza_;
+    Pizza pizza_;
 };
- 
+
 // concrete builder
 class HawaiianPizzaBuilder : public PizzaBuilder {
 public:
-        void buildDough() {
-                pizza_.dough("cross");
-        }
- 
-        void buildSauce() {
-                pizza_.sauce("mild");
-        }
- 
-        void buildTopping() {
-                pizza_.topping("ham+pineapple");
-        }
+    void buildDough()
+    {
+        pizza_.dough("cross");
+    }
+
+    void buildSauce()
+    {
+        pizza_.sauce("mild");
+    }
+
+    void buildTopping()
+    {
+        pizza_.topping("ham+pineapple");
+    }
 };
- 
+
 // concrete builder
 class SpicyPizzaBuilder : public PizzaBuilder {
 public:
-        void buildDough() {
-                pizza_.dough("pan baked");
-        }
- 
-        void buildSauce() {
-                pizza_.sauce("hot");
-        }
- 
-        void buildTopping() {
-                pizza_.topping("pepperoni+salami");
-        }
+    void buildDough()
+    {
+        pizza_.dough("pan baked");
+    }
+
+    void buildSauce()
+    {
+        pizza_.sauce("hot");
+    }
+
+    void buildTopping()
+    {
+        pizza_.topping("pepperoni+salami");
+    }
 };
- 
+
 // Director
 class Cook {
 public:
-        Cook()
-            : pizzaBuilder_(NULL/*nullptr*/)//Chinmay Mandal : nullptr replaced with NULL.
-        {       }
- 
-        ~Cook() {
-                if (pizzaBuilder_)
-                        delete pizzaBuilder_;
-        }
- 
-        void pizzaBuilder(PizzaBuilder* pizzaBuilder) {
-                if (pizzaBuilder_)
-                        delete pizzaBuilder_;
- 
-                pizzaBuilder_ = pizzaBuilder;
-        }
- 
-        const Pizza& getPizza() {
-                return pizzaBuilder_->pizza();
-        }
- 
-        void constructPizza() {
-                pizzaBuilder_->buildDough();
-                pizzaBuilder_->buildSauce();
-                pizzaBuilder_->buildTopping();
-        }
- 
+    Cook()
+        : pizzaBuilder_(NULL /*nullptr*/) // Chinmay Mandal : nullptr replaced with NULL.
+    {
+    }
+
+    ~Cook()
+    {
+        if (pizzaBuilder_)
+            delete pizzaBuilder_;
+    }
+
+    void pizzaBuilder(PizzaBuilder* pizzaBuilder)
+    {
+        if (pizzaBuilder_)
+            delete pizzaBuilder_;
+
+        pizzaBuilder_ = pizzaBuilder;
+    }
+
+    const Pizza& getPizza()
+    {
+        return pizzaBuilder_->pizza();
+    }
+
+    void constructPizza()
+    {
+        pizzaBuilder_->buildDough();
+        pizzaBuilder_->buildSauce();
+        pizzaBuilder_->buildTopping();
+    }
+
 private:
-        PizzaBuilder* pizzaBuilder_;
+    PizzaBuilder* pizzaBuilder_;
 };
- 
 
-int main() {
-        Cook cook;
-        cook.pizzaBuilder(new HawaiianPizzaBuilder);
-        cook.constructPizza();
- 
-        Pizza hawaiian = cook.getPizza();
-        hawaiian.open();
- 
-        cook.pizzaBuilder(new SpicyPizzaBuilder);
-        cook.constructPizza();
- 
-        Pizza spicy = cook.getPizza();
-        spicy.open();
+int main()
+{
+    Cook cook;
+    cook.pizzaBuilder(new HawaiianPizzaBuilder);
+    cook.constructPizza();
+
+    Pizza hawaiian = cook.getPizza();
+    hawaiian.open();
+
+    cook.pizzaBuilder(new SpicyPizzaBuilder);
+    cook.constructPizza();
+
+    Pizza spicy = cook.getPizza();
+    spicy.open();
 }
-
-
-
--- DesignPattern/code/18_Observer.cpp
+++ DesignPattern/code/18_Observer.cpp
@@ -1,77 +1,182 @@
 /*
  * @Author: JohnJeep
- * @Date: 2020-09-17 15:34:22
- * @LastEditTime: 2020-09-17 16:15:41
- * @LastEditors: Please set LastEditors
- * @Description: 观察者模式实现
- * 
+ * @Date: 2021-01-19 15:32:38
+ * @LastEditTime: 2022-07-08 11:27:28
+ * @LastEditors: DESKTOP-0S33AUT
+ * @Description: Oberver 模式再次探究
  */
-#include <iostream>
-#include <cstdio>
 #include <cstring>
+#include <iostream>
 #include <list>
-
 using namespace std;
 
-class Secretary;
-class Stuff
-{
-private:
-   Secretary* m_secretary;
+// abstract obsrver
+class IObserver {
 public:
-    Stuff(Secretary* secretary) 
-    {
-        this->m_secretary = secretary;
-    }
-    ~Stuff() 
-    {
-    }
-    void updataMsg(string info)
-    {
-        cout << info << endl;
-    }
+    IObserver() {};
+    virtual ~IObserver() {};
+
+    // 更新获得的观察目标信息
+    virtual void Update(const string& message_from_subject) = 0;
 };
 
-class Secretary
-{
-private:
-    list<Stuff*> m_st;
+// abstract subject
+class ISubject {
 public:
-    Secretary() 
-    {
-        m_st.clear();   // clear list
+    ISubject() { }
+    virtual ~ISubject() { }
+
+    // 观察目标中添加观察者
+    virtual void Attach(IObserver* observer) = 0;
+
+    // 观察目标中删除观察者
+    virtual void Detach(IObserver* observer) = 0;
+
+    // 观察目标的状态有变化后，通知所有的观察者
+    virtual void Notify() = 0;
+};
+
+// concrete subject
+class Subject : public ISubject {
+public:
+    Subject() { }
+    
+    virtual ~Subject() 
+    { 
+        cout << "Goodbye, I was the Subject." << endl; 
     }
-    ~Secretary() 
-    {}
-    void sendUpdateMsg(Stuff* st)
+
+    void Attach(IObserver* observer) override
     {
-        m_st.push_back(st);
+        m_list_observer.push_back(observer);
     }
-    void notify(string info)
+
+    void Detach(IObserver* observer) override
     {
-        // 给所有的员工发送信息
-        for (list<Stuff*>::iterator iter = m_st.begin(); iter != m_st.end(); iter++)
-        {
-            (*iter)->updataMsg(info);
+        m_list_observer.remove(observer);
+    }
+
+    void CreateMessage(string message = "Empty")
+    {
+        this->m_message = message;
+        Notify();   // 观察目标的状态发生变化后，通知所有已注册的观察者
+    }
+
+    void HowManyObserver()
+    {
+        cout << "There are " << m_list_observer.size()
+             << " observers in the list." << endl;
+    }
+
+    void Notify() override
+    {
+        list<IObserver*>::iterator iterator = m_list_observer.begin();
+        HowManyObserver();
+        while (iterator != m_list_observer.end()) {
+            (*iterator)->Update(m_message);
+            ++iterator;
         }
     }
+
+    /**
+     * Usually, the subscription logic is only a fraction of what a Subject can
+     * really do. Subjects commonly hold some important business logic, that
+     * triggers a notification method whenever something important is about to
+     * happen (or after it).
+     */
+    void SomeBuinessLogic() // no use
+    {
+        this->m_message = "change message";
+        Notify();
+        cout << "I'm about to do something important." << endl;
+    }
+
+private:
+    list<IObserver*> m_list_observer;   // 待注册的观察者
+    string m_message;
 };
 
-int main(int argc, char *argv[])
-{
-    Secretary* hiiro = new Secretary;
-    Stuff* li = new Stuff(hiiro);
-    Stuff* wang = new Stuff(hiiro);
+// concrete oberver
+class Observer : public IObserver {
+public:
+    Observer(Subject& subject)
+        : m_subject(subject)
+    {
+        this->m_subject.Attach(this);
+        cout << "Hi, I'm the observer \"" << ++Observer::m_static_number
+             << "\"." << endl;
 
-    // 直接操作的Secretary的对象去通知Stuff的对象，需要两步操作
-    // 1、Secretary的对象将消息发送给谁？
-    // 2、Secretary的对象发送什么消息？
-    hiiro->sendUpdateMsg(li);
-    hiiro->sendUpdateMsg(wang);
-    hiiro->notify("Living now");
-    delete wang;
-    delete li;
-    delete hiiro;
-    
+        this->m_number = Observer::m_static_number;
+    }
+    virtual ~Observer()
+    {
+        cout << "Goodbye, I was the observer \"" << this->m_number << "\"" << endl;
+    }
+
+    void Update(const string& message_from_subject) override
+    {
+        m_message_from_subject = message_from_subject;
+        PrintInfo();
+    }
+
+    void PrintInfo()
+    {
+        cout << "Observer \"" << this->m_number
+             << "\" : a new message is available --> "
+             << this->m_message_from_subject << endl;
+    }
+
+    void RemoveMeFromList()
+    {
+        m_subject.Detach(this); // this: 运行时，解析的是哪个就传入的是哪个
+        cout << "Observer \"" << this->m_number << " \" remove from list." << endl;
+    }
+
+private:
+    int m_number;
+    Subject& m_subject;
+    string m_message_from_subject;
+    static int m_static_number;
+};
+
+// init static
+int Observer::m_static_number = 0;
+
+void ClientCode()
+{
+    Subject* sub = new Subject;
+    Observer* observer1 = new Observer(*sub);
+    Observer* observer2 = new Observer(*sub);
+    Observer* observer3 = new Observer(*sub);
+    Observer* observer4;
+    Observer* observer5;
+
+    sub->CreateMessage("Hello everyone!");
+    observer3->RemoveMeFromList();
+
+    sub->CreateMessage("The weather is hot today!");
+    observer4 = new Observer(*sub);
+
+    observer2->RemoveMeFromList();
+    observer5 = new Observer(*sub);
+
+    sub->CreateMessage("I'm very hot!");
+    observer5->RemoveMeFromList();
+
+    observer4->RemoveMeFromList();
+    observer1->RemoveMeFromList();
+
+    delete sub;
+    delete observer1;
+    delete observer2;
+    delete observer3;
+    delete observer4;
+    delete observer5;
+}
+
+int main()
+{
+    ClientCode();
+
     return 0;
 }
