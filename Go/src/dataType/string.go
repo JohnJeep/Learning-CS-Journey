ed6848884a2dc19f6979621a51e650dcd117372b
JohnJeep <JohnJeep1985@gail.com>
2022-12-31 17:34:36 +0800

    docs(Go): 标准库strings API接口用法

--- Go/src/dataType/string.go
+++ Go/src/dataType/string.go
@@ -6,8 +6,19 @@ import (
 )
 
 func main() {
-
+	// UTF-8 中一个ASCII码占一个字节，汉字占三个字节
+	s := "你好"
+	fmt.Println("s len:", len(s))
 	fmt.Println(strings.Contains("contain", "foo"))
 	fmt.Println(strings.Contains("contain", "tain"))
 
+	var bytes = []byte("hello")
+	fmt.Printf("bytes: %v\n", bytes) // 显示的是字符的 ASCII 码
+
+	// str := string([]byte{97, 98, 99})
+	str := string([]byte{97, 98, 99})
+	fmt.Printf("byte convert string: %v\n", str)
+
+	comp := strings.EqualFold("xyz", "XYZ") // 不区分大小写
+	fmt.Println("two string compare:", comp)
 }
