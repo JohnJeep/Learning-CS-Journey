69e7b42ac53f3119b6aada970f64651c38b872bc
JohnJeep <JohnJeep1985@gmail.com>
2025-04-17 18:29:17 +0800

    feat(go): add link_list algorithm

---
+++ Go/proj-go/algorithm/link_list.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: JohnJeep
+ * @Date: 2025-04-17 16:31:53
+ * @LastEditors: JohnJeep
+ * @LastEditTime: 2025-04-17 16:32:08
+ * @Description: link lis implementation
+ * Copyright (c) 2025 by John Jeep, All Rights Reserved.
+ */
+package algorithm
